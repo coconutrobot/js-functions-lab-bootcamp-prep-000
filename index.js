ff8afbb4fb7555b45453d75bf4d34873f8060f11
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-18 01:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!';
 }
 
-happyHolidaysTo("you");
-
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!';
 }
